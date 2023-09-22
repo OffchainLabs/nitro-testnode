87088c8451c44611ef7b00fe4d0fb545ce610561
Joshua Colvin <jcolvin@offchainlabs.com>
2023-09-21 21:15:32 -0700

    Use eth-global stylus docker image
    Use patched version of Stylus that allows for up to 128MB contract size for eth-global hackathon

--- test-node.bash
+++ test-node.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-NITRO_NODE_VERSION=offchainlabs/stylus-node:v0.1.0-f47fec1-dev
+NITRO_NODE_VERSION=offchainlabs/stylus-node:v0.eth-global-2442a52
 BLOCKSCOUT_VERSION=offchainlabs/blockscout:v1.0.0-c8db5b1
 
 mydir=`dirname $0`
