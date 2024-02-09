52376108db3b8433b692f9d96a99dd80c3a50b7a
Nodar Ambroladze <nodar.ambroladze@gmail.com>
2024-02-09 17:15:31 +0100

    Fix script path in testnode.bash

--- .github/workflows/testnode.bash
+++ .github/workflows/testnode.bash
@@ -2,7 +2,7 @@
 # The script starts up the test node (with timeout 1 minute), with option to
 # run l2 transactions to make sure node is working
 
-timeout 60 ./nitro-testnode/test-node.bash --init --dev || exit_status=$?
+timeout 60 ${{ github.workspace }}/nitro-testnode/test-node.bash --init --dev || exit_status=$?
 
 if  [ -n "$exit_status" ] && [ $exit_status -ne 0 ] && [ $exit_status -ne 124 ]; then
     echo "Testnode failed."
