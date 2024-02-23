74e07df5ad1291bfe079f0975ffb112096b93695
Nodar Ambroladze <nodar.ambroladze@gmail.com>
2024-02-23 16:34:34 +0100

    Increase timeout

--- .github/workflows/testnode.bash
+++ .github/workflows/testnode.bash
@@ -2,8 +2,8 @@
 # The script starts up the test node (with timeout 1 minute), with option to
 # run l2 transactions to make sure node is working
 
-timeout 10m ${GITHUB_WORKSPACE}/test-node.bash --init --detach
-sleep 10m
+timeout 20m ${GITHUB_WORKSPACE}/test-node.bash --init --detach
+sleep 20m
 ${GITHUB_WORKSPACE}/test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait  || exit_status=$?
 
 if  [ -n "$exit_status" ] && [ $exit_status -ne 0 ] && [ $exit_status -ne 124 ]; then
