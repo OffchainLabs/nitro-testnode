ff7ff3ac451f966e78e4df45e665bb7bd52b2861
Nodar Ambroladze <nodar.ambroladze@gmail.com>
2024-02-23 16:09:12 +0100

    Run testnode using latest release (without --dev)

--- .github/workflows/testnode.bash
+++ .github/workflows/testnode.bash
@@ -2,9 +2,9 @@
 # The script starts up the test node (with timeout 1 minute), with option to
 # run l2 transactions to make sure node is working
 
-timeout 60 ${GITHUB_WORKSPACE}/test-node.bash --init --dev --detach || exit_status=$?
-sleep 15m
-${GITHUB_WORKSPACE}/test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait
+timeout 10m ${GITHUB_WORKSPACE}/test-node.bash --init --detach
+sleep 10m
+${GITHUB_WORKSPACE}/test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait  || exit_status=$?
 
 if  [ -n "$exit_status" ] && [ $exit_status -ne 0 ] && [ $exit_status -ne 124 ]; then
     echo "Testnode failed."
