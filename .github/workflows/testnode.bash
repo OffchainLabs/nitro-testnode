9d527abe7c8215cee1308730d3cd8a05997ed223
Diego Ximenes <dxmendes1@gmail.com>
2024-06-21 15:36:20 -0300

    Runs CI with l3node and no-simple

--- .github/workflows/testnode.bash
+++ .github/workflows/testnode.bash
@@ -1,20 +1,35 @@
 #!/bin/bash
-# The script starts up the test node and waits until the timeout (10min) or 
+# The script starts up the test node and waits until the timeout (10min) or
 # until send-l2 succeeds.
 
 # Start the test node and get PID, to terminate it once send-l2 is done.
 cd ${GITHUB_WORKSPACE}
 
 # TODO once develop is merged into nitro-contract's master, remove the NITRO_CONTRACTS_BRANCH env var
-./test-node.bash --init-force --detach
+./test-node.bash --init-force --l3node --no-simple --detach
 
 START=$(date +%s)
-SUCCEDED=0
+L2_TRANSACTION_SUCCEEDED=false
+L3_TRANSACTION_SUCCEEDED=false
+SUCCEEDED=false
 
 while true; do
-    if ${GITHUB_WORKSPACE}/test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait; then
-        echo "Sending l2 transaction succeeded"
-        SUCCEDED=1
+    if [ "$L2_TRANSACTION_SUCCEEDED" = false ]; then
+        if ${GITHUB_WORKSPACE}/test-node.bash script send-l2 --ethamount 100 --to user_l2user --wait; then
+            echo "Sending l2 transaction succeeded"
+            L2_TRANSACTION_SUCCEEDED=true
+        fi
+    fi
+
+    if [ "$L3_TRANSACTION_SUCCEEDED" = false ]; then
+        if ${GITHUB_WORKSPACE}/test-node.bash script send-l3 --ethamount 100 --to user_l3user --wait; then
+            echo "Sending l3 transaction succeeded"
+            L3_TRANSACTION_SUCCEEDED=true
+        fi
+    fi
+
+    if [ "$L2_TRANSACTION_SUCCEEDED" = true ] && [ "$L3_TRANSACTION_SUCCEEDED" = true ]; then
+        SUCCEEDED=true
         break
     fi
 
@@ -31,7 +46,7 @@ done
 
 docker-compose stop
 
-if [ "$SUCCEDED" -eq 0 ]; then
+if [ "$SUCCEEDED" = false ]; then
     docker-compose logs
     exit 1
 fi
