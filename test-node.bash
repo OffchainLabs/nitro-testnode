4732c5d0278e85e291b9d425b7a6361be1bcae25
Tristan Wilson <twilson@offchainlabs.com>
2024-08-13 16:45:57 +0200

    Declare variable before use for -u flag

--- test-node.bash
+++ test-node.bash
@@ -461,6 +461,7 @@ if $force_init; then
         echo l3owneraddress $l3owneraddress
         docker compose run scripts --l2owner $l3owneraddress  write-l3-chain-config
 
+        EXTRA_L3_DEPLOY_FLAG=""
         if $l3_custom_fee_token; then
             echo == Deploying custom fee token
             nativeTokenAddress=`docker compose run scripts create-erc20 --deployer user_fee_token_deployer --mintTo user_token_bridge_deployer --bridgeable $tokenbridge | tail -n 1 | awk '{ print $NF }'`
