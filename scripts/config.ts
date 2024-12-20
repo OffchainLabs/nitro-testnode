d86f5b69a866d809647a28fb48249f80f6393203
Chris Buckland <cpbuckland88@gmail.com>
2024-12-20 12:47:35 +0000

    Revert rollup change

--- scripts/config.ts
+++ scripts/config.ts
@@ -177,7 +177,6 @@ function writeConfigs(argv: any) {
     const valJwtSecret = path.join(consts.configpath, "val_jwt.hex")
     const chainInfoFile = path.join(consts.configpath, "l2_chain_info.json")
     let baseConfig = {
-        "ensure-rollup-deployment": false,
         "parent-chain": {
             "connection": {
                 "url": argv.l1url,
