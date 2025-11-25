1b0ccca664a8d5704522e5ab4a0cea2769946c51
TucksonDev <tucksondev@gmail.com>
2025-11-25 13:08:02 +0000

    Improve BoLD config to speed up confirmation of assertions

--- scripts/config.ts
+++ scripts/config.ts
@@ -201,7 +201,9 @@ function writeConfigs(argv: any) {
         "node": {
             "bold": {
                 "rpc-block-number": "latest",
-                "assertion-posting-interval": "10s"
+                "assertion-posting-interval": "10s",
+                "assertion-confirming-interval": "10s",
+                "parent-chain-block-time": 1,
             },
             "staker": {
                 "dangerous": {
