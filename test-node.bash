2fbe71f55bd05d118375b4f9a41882707d63cc57
gzeon <im@gzeon.dev>
2026-03-20 14:16:51 +0000

    chore: pin to v3.10.0-rc.5 + enable WAVM prover

--- test-node.bash
+++ test-node.bash
@@ -2,7 +2,7 @@
 
 set -eu
 
-: ${NITRO_NODE_VERSION:=offchainlabs/nitro-node:v3.9.6-91bf578}
+: ${NITRO_NODE_VERSION:=offchainlabs/nitro-node:v3.10.0-rc.5-b9d1452-dev}
 BLOCKSCOUT_VERSION=offchainlabs/blockscout:v1.1.0-0e716c8
 
 # nitro-contract workaround for testnode
